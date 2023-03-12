febaead9eb68a2c20c8ff677db6de626519db026
“Bryan” <velicka.b@gmail.com>
2023-03-12 21:06:48 +0100

    Reducido el tamaño del modelo de mensaje

--- lib/models/mensaje.dart
+++ lib/models/mensaje.dart
@@ -1,22 +1,22 @@
 class Mensaje {
   final int id;
-  final int idChat;
+  //final int idChat;
   final String idUsuarioCreador;
   final String contenido;
-  final String? creadoEn;
+  //final String? creadoEn;
 
   Mensaje({
     required this.id,
-    required this.idChat,
+    //required this.idChat,
     required this.idUsuarioCreador,
     required this.contenido,
-    this.creadoEn,
+    //this.creadoEn,
   });
 
   Mensaje.fromKeyValue(Map<String, dynamic> json)
       : id = json['id'],
-        idChat = json['chat_id'],
+        //idChat = json['chat_id'],
         idUsuarioCreador = json['creador'],
-        contenido = json['contenido'],
-        creadoEn = json['created_at'];
+        contenido = json['contenido'];
+  //creadoEn = json['created_at'];
 }
